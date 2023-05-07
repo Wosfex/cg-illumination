8007f01678037f6cc0e754cb31884b00888e1bae
Stei7041 <stei7041@stthomas.edu>
2023-05-07 18:45:55 -0500

    Scene 1 done

--- src/renderer.js
+++ src/renderer.js
@@ -3,6 +3,8 @@ import { UniversalCamera } from '@babylonjs/core/Cameras/universalCamera';
 import { PointLight } from '@babylonjs/core/Lights/pointLight';
 import { CreateSphere } from '@babylonjs/core/Meshes/Builders/sphereBuilder';
 import { Texture } from '@babylonjs/core/Materials/Textures/texture';
+import { Mesh } from '@babylonjs/core/Meshes/mesh';
+import { VertexData } from '@babylonjs/core/';
 import { RawTexture } from '@babylonjs/core/Materials/Textures/rawTexture';
 import { Color3, Color4 } from '@babylonjs/core/Maths/math.color';
 import { Vector2, Vector3 } from '@babylonjs/core/Maths/math.vector';
@@ -209,14 +211,14 @@ class Renderer {
         current_scene.camera.maxZ = 100.0;
 
         // Create point light sources
-        let light0 = new PointLight('light0', new Vector3(1.0, 1.0, 5.0), scene);
-        light0.diffuse = new Color3(1.0, 1.0, 1.0);
+        let light0 = new PointLight('light0', new Vector3(1.0, 60.0, 70.0), scene);
+        light0.diffuse = new Color3(1.0, 1.0, 0.0);
         light0.specular = new Color3(1.0, 1.0, 1.0);
         current_scene.lights.push(light0);
 
-        let light1 = new PointLight('light1', new Vector3(0.0, 3.0, 0.0), scene);
-        light1.diffuse = new Color3(1.0, 1.0, 1.0);
-        light1.specular = new Color3(1.0, 1.0, 1.0);
+        let light1 = new PointLight('light1', new Vector3(0.0, 3.0, 15.0), scene);
+        light1.diffuse = new Color3(-0.6627, -0.6627, -0.6627);
+        light1.specular = new Color3(-0.6627, -0.6627, -0.6627);
         current_scene.lights.push(light1);
 
         // Create ground mesh
@@ -224,7 +226,7 @@ class Renderer {
         let ground_heightmap = new Texture('/heightmaps/default.png', scene);
         ground_mesh.scaling = new Vector3(20.0, 1.0, 20.0);
         ground_mesh.metadata = {
-            mat_color: new Color3(0.10, 0.65, 0.15),
+            mat_color: new Color3(0.10, 0.45, 0.15),
             mat_texture: white_texture,
             mat_specular: new Color3(0.0, 0.0, 0.0),
             mat_shininess: 1,
@@ -234,9 +236,9 @@ class Renderer {
         }
         ground_mesh.material = materials['ground_' + this.shading_alg];
 
-        // Create other models
+        // Create other models //
         let cloud1 = CreateSphere('sphere', {segments: 20}, scene);
-        cloud1.position = new Vector3(1.0, 4.5, 3.0);
+        cloud1.position = new Vector3(1.0, 7.5, 3.0);
         cloud1.metadata = {
             mat_color: new Color3(1.0, 1.0, 1.0),
             mat_texture: white_texture,
@@ -248,7 +250,7 @@ class Renderer {
         current_scene.models.push(cloud1);
     
         let cloud2 = CreateSphere('sphere', {segments: 20}, scene);
-        cloud2.position = new Vector3(0.5, 4.5, 3.0);
+        cloud2.position = new Vector3(0.5, 7.5, 3.0);
         cloud2.metadata = {
             mat_color: new Color3(1.0, 1.0, 1.0),
             mat_texture: white_texture,
@@ -260,7 +262,7 @@ class Renderer {
         current_scene.models.push(cloud2);
 
         let cloud3 = CreateSphere('sphere', {segments: 20}, scene);
-        cloud3.position = new Vector3(0.3, 4.5, 2.5);
+        cloud3.position = new Vector3(0.3, 7.5, 2.5);
         cloud3.metadata = {
             mat_color: new Color3(1.0, 1.0, 1.0),
             mat_texture: white_texture,
@@ -272,7 +274,7 @@ class Renderer {
         current_scene.models.push(cloud3);
 
         let cloud4 = CreateSphere('sphere', {segments: 20}, scene);
-        cloud4.position = new Vector3(0.7, 4.8, 2.5);
+        cloud4.position = new Vector3(0.7, 7.8, 2.5);
         cloud4.metadata = {
             mat_color: new Color3(1.0, 1.0, 1.0),
             mat_texture: white_texture,
@@ -284,7 +286,7 @@ class Renderer {
         current_scene.models.push(cloud4);
 
         let cloud5 = CreateSphere('sphere', {segments: 20}, scene);
-        cloud5.position = new Vector3(1.0, 4.5, 3.5);
+        cloud5.position = new Vector3(1.0, 7.5, 3.5);
         cloud5.metadata = {
             mat_color: new Color3(1.0, 1.0, 1.0),
             mat_texture: white_texture,
@@ -296,7 +298,7 @@ class Renderer {
         current_scene.models.push(cloud5);
 
         let cloud6 = CreateSphere('sphere', {segments: 20}, scene);
-        cloud6.position = new Vector3(0.8, 4.5, 2.0);
+        cloud6.position = new Vector3(0.8, 7.5, 2.0);
         cloud6.metadata = {
             mat_color: new Color3(1.0, 1.0, 1.0),
             mat_texture: white_texture,
@@ -306,6 +308,79 @@ class Renderer {
         }
         cloud6.material = materials['illum_' + this.shading_alg];
         current_scene.models.push(cloud6);
+
+        let customHouse = new Mesh("custom", scene);
+        let housePositions = [
+            // Walls
+            0,0,0, 4,0,0, 4,4,0, // 0,1,2
+            0,0,0, 0,4,0, 4,4,0, // 3,4,5
+
+            0,0,0, 0,0,4, 0,4,4, // 6,7,8
+            0,0,0, 0,4,0, 0,4,4, // 9,10,11
+
+            0,0,4, 4,0,4, 4,4,4, // 12,13,14
+            0,0,4, 0,4,4, 4,4,4, // 15,16,17
+
+            4,0,4, 4,4,4, 4,0,0, // 18,19,20
+            4,4,4, 4,4,0, 4,0,0, // 21,22,23  
+            
+            // Roof triangles
+            0,4,0, 4,4,0, 2,6,0, // 24,25,26
+            4,4,4, 0,4,4, 2,6,4, // 27,28,29
+
+            // roof
+            0,4,0, 0,4,4, 2,6,4, // 30,31,32
+            0,4,0, 2,6,4, 2,6,0, // 33,34,35
+
+            4,4,0, 4,4,4, 2,6,0, // 36,37,38
+            4,4,4, 2,6,4, 2,6,0, // 39,40,41
+
+            // doorway top
+            1,3,0, 1,3,-1, 3,3,0, // 42,43,44
+            3,3,0, 3,3,-1, 1,3,-1, // 45,46,47
+
+            //doorway sides
+            1,3,0, 1,0,0, 1,0,-1, // 48,49,50
+            1,3,0, 1,3,-1, 1,0,-1, // 51,52,53
+
+            3,3,0, 3,0,0, 3,0,-1, // 54,55,56
+            3,3,0, 3,3,-1, 3,0,-1 // 57,58,59
+        ]
+        let houseIndices = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59];
+
+        let houseNormals = [];
+        
+        customHouse.metadata = {
+            mat_color: new Color3(0.66, 0.66, 0.66),
+            mat_texture: white_texture,
+            mat_specular: new Color3(0.5, 0.5, 0.5),
+            mat_shininess: 16,
+            texture_scale: new Vector2(1.0, 1.0)
+        };
+
+        // Imported VertexData, found guide online about how to calculate normals automatically
+        VertexData.ComputeNormals(housePositions, houseIndices, houseNormals);
+        var vertexData = new VertexData();
+        vertexData.positions = housePositions;
+        vertexData.indices = houseIndices;
+        vertexData.normals = houseNormals;
+        vertexData.applyToMesh(customHouse, true);
+
+        customHouse.material = materials['illum_' + this.shading_alg];
+        current_scene.models.push(customHouse);
+
+        // Imported VertexData, found guide online about how to calculate normals automatically
+        VertexData.ComputeNormals(rainPositions, rainIndices, rainNormals);
+        var vertexData = new VertexData();
+        vertexData.positions = rainPositions;
+        vertexData.indices = rainIndices;
+        vertexData.normals = rainNormals;
+        vertexData.applyToMesh(customRain, true);
+
+        customRain.material = materials['illum_' + this.shading_alg];
+        current_scene.models.push(customRain);
+
+        
         
         // Added code, starter taken from a guide on babylon.js key list
         scene.onKeyboardObservable.add((kbInfo) => {
@@ -619,7 +694,6 @@ class Renderer {
         });
     }
 
-    
 
     updateShaderUniforms(scene_idx, shader) {
         let current_scene = this.scenes[scene_idx];
