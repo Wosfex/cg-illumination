b3162582460b4f726119c123a218606328c376ed
Stei7041 <stei7041@stthomas.edu>
2023-05-07 20:04:06 -0500

    build v2

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/cg-illumination/',
+  base: 'stei7041/cg-illumination/',
   plugins: [vue()],
   resolve: {
     alias: {
